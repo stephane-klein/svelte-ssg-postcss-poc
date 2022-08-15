951cb44d1b440f9c97bbde6850d45cf2bd03985b
Stéphane Klein <contact@stephane-klein.info>
2022-08-15 17:42:28 +0200

    Remove useless config un svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-import postcssNormalize from 'postcss-normalize';
-import postcssNested from 'postcss-nested';
-import autoprefixer from 'autoprefixer';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: [
         preprocess({
-            postcss: {
-                plugins: [
-                    postcssNormalize(),
-                    postcssNested(),
-                    autoprefixer()
-                ]
-            }
+            postcss: {}
         })
     ],
 
