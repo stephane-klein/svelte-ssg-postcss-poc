a1d448d544e3f5b51b0998a6d99eade0e91f937f
Stéphane Klein <contact@stephane-klein.info>
2022-10-30 18:03:23 +0100

    Remove typescript stuff

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,4 +1,4 @@
-module.exports = ({ env }) => ({
+module.exports = () => ({
     map: true,
     plugins: {
         'postcss-normalize': {},
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: [
         preprocess({
