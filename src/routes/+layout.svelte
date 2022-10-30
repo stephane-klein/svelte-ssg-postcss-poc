642b907ba0df760443b9f2e4dbf6d72408f2abae
Stéphane Klein <contact@stephane-klein.info>
2022-10-30 18:09:41 +0100

    WIP

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
-<style src="../app.css"></style>
 
 <article>
     <div class="box-red">box red</div>
     <slot />
+    <div class="box-yellow">box yellow</div>
 </article>
+
+<style lang="postcss">
+:global {
+    @import '../app.css';
+}
+
+.box-yellow {
+    background-color: yellow;
+}
+</style>
